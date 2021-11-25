77361b1888f37b7a9d38e2cb88539bc9cb7d18e6
tiagooogomes <91339622+tiagooogomes@users.noreply.github.com>
2021-11-25 09:24:20 -0300

    Final

--- script.js
+++ script.js
@@ -3,34 +3,24 @@ var botaoMultiplocar = document.querySelector("#botaoMultiplicar");
 var botaoDividir = document.querySelector("#botaoDividir");
 var botaoSubtrair = document.querySelector("#botaoSubtrair");
 
-
-
-
 botaoSomar.addEventListener("click", function(event){
     event.preventDefault();
 
     var form = document.querySelector("#formulario");
-
     var campo1 = parseInt(form.campo1.value);
     var campo2 = parseInt(form.campo2.value);
-
     var resultadoSomar = campo1 + campo2;
-    
     resultado.innerHTML = resultadoSomar;
 
 });
-
 
 botaoMultiplocar.addEventListener("click", function(event){
     event.preventDefault();
 
     var form = document.querySelector("#formulario");
-
     var campo1 = form.campo1.value;
     var campo2 = form.campo2.value;
-
     var resultadoMultiplicar = campo1 * campo2;
-    
     resultado.innerHTML = resultadoMultiplicar;
 
 });
@@ -39,12 +29,9 @@ botaoDividir.addEventListener("click", function(event){
     event.preventDefault();
 
     var form = document.querySelector("#formulario");
-
     var campo1 = form.campo1.value;
     var campo2 = form.campo2.value;
-
     var resultadoDividir = campo1 / campo2;
-    
     resultado.innerHTML = resultadoDividir;
 
 });
@@ -53,12 +40,9 @@ botaoSubtrair.addEventListener("click", function(event){
     event.preventDefault();
 
     var form = document.querySelector("#formulario");
-
     var campo1 = form.campo1.value;
     var campo2 = form.campo2.value;
-
     var resultadoSubtrair = campo1 - campo2;
-    
     resultado.innerHTML = resultadoSubtrair;
 
 });
